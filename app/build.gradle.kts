c66d1c66bfd101d5f2331f886e83e1995f6416ef
zaouimehdi556-design <zaouimehdi556@gmail.com>
2026-09-06 12:08:14 +0100

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,6 +16,16 @@ android {
         versionName = "1.0"
     }
 
+    // توحيد Java و Kotlin على JVM 17
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+
     buildFeatures {
         compose = true
     }
